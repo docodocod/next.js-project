d33f3ce07a4f988c25e834ee8c421970f297f656
donga <dongan0618@naver.com>
2024-06-05 23:49:08 +0900

    firestore update

--- data/firestore.js
+++ data/firestore.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getDocs,collection,doc,Timestamp,setDoc,getFirestore,getDoc,deleteDoc} from "firebase/firestore"
+import {getDocs,collection,doc,Timestamp,setDoc,getFirestore,getDoc,deleteDoc,updateDoc} from "firebase/firestore"
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
@@ -14,6 +14,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const createdAtTimestamp=Timestamp.fromDate(new Date())
+
 
 //모든 할일 가져오기
 export async function getAllTodos() {
@@ -21,7 +23,7 @@ export async function getAllTodos() {
     if (querySnapshot.empty){
         return [];
     }
-    const Todos=[];
+    const allTodos=[];
     querySnapshot.forEach((doc) => {
         const aTodo={
             id: doc.id,
@@ -29,23 +31,27 @@ export async function getAllTodos() {
             is_done: doc.data()["is_done"],
             created_at: doc.data()["created_at"].toDate().toLocaleTimeString('ko')
         }
-        Todos.push(aTodo);
+        allTodos.push(aTodo);
     })
-    return Todos;
+    return allTodos;
 }
 
 //단일 할일 추가
 export async function addTodo({title}){
     const newTodoRef=doc(collection(db,"transfer-todo"));
-    const createdAtTimestamp=Timestamp.fromDate(new Date())
     const newTodoData={
-        id:newTodoRef,
+        id:newTodoRef.id,
         title:title,
         is_done:false,
         created_at:createdAtTimestamp
     }
     await setDoc(newTodoRef,newTodoData);
-    return newTodoData;
+    return {
+        id:newTodoRef.id,
+        title:title,
+        is_done:false,
+        created_at:createdAtTimestamp.toDate()
+    }
 }
 
 //단일 할일 조회
@@ -57,13 +63,15 @@ export async function getTodo(id){
     const todoDocSnap=await getDoc(TodoRef)
 
     if(todoDocSnap.exists()){
-        const getTodo={
+        console.log('Document data:', todoDocSnap.data());
+
+        const getATodo={
             id: todoDocSnap.id,
             title: todoDocSnap.data()['title'],
             is_done: todoDocSnap.data()['is_done'],
             created_at: todoDocSnap.data()["created_at"].toDate()
         }
-        return getTodo;
+        return getATodo;
     }else{
         console.log("no such document");
         return null
@@ -71,22 +79,27 @@ export async function getTodo(id){
 }
 
 //단일 할일 수정
-export async function editTodo(id,title,is_done){
-    if (id===null){
+export async function editTodo(id,{title,is_done}){
+
+    const getTodoList=await getTodo(id);
+
+    if (getTodoList===null){
         return null;
     }
-    const editTimeStamp=Timestamp.fromDate(new Date());
     const TodoRef=doc(db,"transfer-todo",id);
-    const editTodoList={
+    const updatedTodo=await updateDoc(TodoRef,{
+        title: title,
+        is_done: is_done
+    });
+    return {
         id:id,
         title:title,
         is_done:is_done,
-        created_at:editTimeStamp
-    }
-    await setDoc(TodoRef,editTodoList);
-    return editTodoList;
+        created_at:getTodoList.created_at
+    };
 }
 
+//단일 할일 삭제
 export async function deleteTodo(id){
     const getTodoList=await getTodo(id);
 
