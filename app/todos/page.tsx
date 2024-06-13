b9bba1d0c65824923636ededddadb25739d17c93
donga <dongan0618@naver.com>
2024-06-13 18:08:25 +0900

    배포수정

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -4,14 +4,19 @@ import TodosTable from "@/components/todos-table";
 async function getAllTodosApi(){
     const res=await fetch(`${process.env.BASE_URL}/api/todos`,{cache:'no-store'})
     console.log("getAllTodo List")
+    const contentTypeHeaderValue=res.headers.get('Content-Type');
+    if(contentTypeHeaderValue?.includes('text/html'){
+        return null;
+    }
     return res.json();
 }
 export default async function TodosPage(){
     const response=await getAllTodosApi();
+    const fetchedTodos=response?.data ?? [];
   return (
     <div className="flex flex-col space-y-8">
       <h1 className={title()}>Todos</h1>
-        <TodosTable todos={response.data ?? []}/>
+        <TodosTable todos={fetchedTodos}/>
     </div>
   );
 }
