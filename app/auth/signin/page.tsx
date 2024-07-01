3c3115e6748e7ea998edef129026f406166b4050
donga <dongan0618@naver.com>
2024-07-01 23:37:54 +0900

    user update

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,12 @@
+import { title } from "@/components/primitives";
+import LoginComponent from "@/components/loginComponent";
+
+export default async function LoginPage(){
+
+  return (
+    <div className="flex flex-col space-y-8">
+      <h1 className={title()}></h1>
+        <LoginComponent/>
+    </div>
+  );
+}
---
+++ components/userStateBar.tsx
@@ -0,0 +1,5 @@
+export default function userBar() {
+  return <div className="flex gap-4 items-center">
+    <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-20 h-20 text-large" />
+  </div>
+}
